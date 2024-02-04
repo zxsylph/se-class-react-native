f548fc3dbe354b5770f1e0b9b3a7387a765274e9
wiroon.sri@C-709 <wiroon.sri@innosoft.kmutt.ac.th>
2024-02-04 09:44:46 +0700

    get location but test on web only

--- src/components/Jotai2.tsx
+++ src/components/Jotai2.tsx
@@ -8,16 +8,20 @@ function Jotai2(props) {
   const { navigation } = props
   const [count, setCounter] = useAtom(countAtom)
   const onClick = () => setCounter((prev) => prev + 1)
-  const onLink = () => {
+  const onLinkToJotai = () => {
     navigation.navigate('Jotai', { name: 'Jane' })
+  }
+  const onLinkToMyLocation = () => {
+    navigation.navigate('Location', { name: 'Jane' })
   }
 
   return (
     <>
       <Text>Count: {count}</Text>
       <Text>Open up App.js to start working on your app!!!</Text>
-      <Button onPress={onLink} title="Link"></Button>
+      <Button onPress={onLinkToJotai} title="Jotai"></Button>
       <Button onPress={onClick} title="Press me"></Button>
+      <Button onPress={onLinkToMyLocation} title="My Location"></Button>
     </>
   )
 }
--- src/components/MyLocation.tsx
+++ src/components/MyLocation.tsx
@@ -1,13 +1,34 @@
 import Geolocation from '@react-native-community/geolocation'
-import { Text } from 'react-native'
-
-Geolocation.getCurrentPosition((info) => console.log(info))
+import { useEffect, useState } from 'react'
+import { Button, Text } from 'react-native'
 
 function MyLocation() {
+  console.log('MyLocation')
+
+  const [location, setLocation] = useState<any>(null)
+
+  useEffect(() => {
+    console.log('MyLocation useEffect')
+
+    Geolocation.getCurrentPosition((info) => {
+      console.log('info', info)
+      setLocation(info)
+    })
+  }, [])
+
+  useEffect(() => {
+    console.log('location', location)
+  }, [location])
+
+  const onPress = () => {
+    setLocation(null)
+  }
+
   return (
     <>
       <Text>Geo Location</Text>
-      <Text></Text>
+      <Text>{location?.coords?.latitude}</Text>
+      <Button onPress={onPress} title="Set Null"></Button>
     </>
   )
 }
